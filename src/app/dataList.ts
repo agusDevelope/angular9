5de47bc719344a30d106d64147e2d71be6e5bec4
Agustín Curiel Tapia "Arri Tín Tín <acurielt@caprepoli.cdmx.gob.mx>
2021-10-13 14:38:27 -0500

    tabla con datos de servicio

--- src/app/dataList.ts
+++ src/app/dataList.ts
@@ -1,6 +1,6 @@
 import { Data } from './data';
 
-export const betsellerList: Data[] = [
+export const dataList: Data[] = [
     { nombre: 'José Pérez', curp: 'JOPE910112HDFRPG00', rfc: 'JOPE910112', edad: 25},
     {nombre: 'Juan Escutia', curp: 'JAES840512HDFRPG00', rfc: 'JAES840512', edad: 35},
     { nombre: 'Martha Rodríguez', curp: 'MARO740512HDFRPG00', rfc: 'MARO740512', edad: 45},
